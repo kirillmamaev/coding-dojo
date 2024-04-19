732e0f3575d4f083d7b8a49788f5797a59b7e0ed
Kirill Mamaev <kirill.mamaev@mable.com.au>
2024-04-19 12:33:27 +1000

    Add 12th verse test for TwelveDaysOfChristmas

--- Ruby/Lib/twelve_days_of_christmas/twelve_days_of_christmas.rb
+++ Ruby/Lib/twelve_days_of_christmas/twelve_days_of_christmas.rb
@@ -3,25 +3,57 @@ module TwelveDaysOfChristmas
     def initialize(locale: :en)
       @locale = locale
     end
-    
+
     def verse(number)
       if number == 1
-        "On the first day of Christmas/n" \
-        "My true love gave to me:/n" \
+        generate_verse_lines(number) +
         "A #{DICTIONARY[@locale][:gifts][1]}."
       elsif number == 2
-        "On the second day of Christmas/n" \
-        "My true love gave to me:/n" \
-        "Two turtle doves,/n" \
+        generate_verse_lines(number) +
+        "Two #{DICTIONARY[@locale][:gifts][2]},\n" \
         "And a #{DICTIONARY[@locale][:gifts][1]}."
-      else
-        "On the third day of Christmas/n" \
-        "My true love gave to me:/n" \
-        "Three french hens,/n" \
-        "Two turtle doves,/n" \
+      elsif number == 3
+        generate_verse_lines(number) +
+        "Three #{DICTIONARY[@locale][:gifts][3]},\n" \
+        "Two #{DICTIONARY[@locale][:gifts][2]},\n" \
+        "And a #{DICTIONARY[@locale][:gifts][1]}."
+      elsif number == 12
+        generate_verse_lines(number) +
+        "Twelve #{DICTIONARY[@locale][:gifts][12]},\n" \
+        "Eleven #{DICTIONARY[@locale][:gifts][11]},\n" \
+        "Ten #{DICTIONARY[@locale][:gifts][10]},\n" \
+        "Nine #{DICTIONARY[@locale][:gifts][9]},\n" \
+        "Eight #{DICTIONARY[@locale][:gifts][8]},\n" \
+        "Seven #{DICTIONARY[@locale][:gifts][7]},\n" \
+        "Six #{DICTIONARY[@locale][:gifts][6]},\n" \
+        "Five #{DICTIONARY[@locale][:gifts][5]},\n" \
+        "Four #{DICTIONARY[@locale][:gifts][4]},\n" \
+        "Three #{DICTIONARY[@locale][:gifts][3]},\n" \
+        "Two #{DICTIONARY[@locale][:gifts][2]},\n" \
         "And a #{DICTIONARY[@locale][:gifts][1]}."
       end
     end
+
+    def generate_verse_lines(number)
+      "On the #{LINE_NUMBER[number]} day of Christmas\n" \
+      "My true love gave to me:\n" \
+    end
+
+    LINE_NUMBER = {
+      0 => nil,
+      1 => "first",
+      2 => "second",
+      3 => "third",
+      4 => "fourth",
+      5 => "fifth",
+      6 => "sixth",
+      7 => "seventh",
+      8 => "eighth",
+      9 => "ninth",
+      10 => "tenth",
+      11 => "eleventh",
+      12 => "twelfth"
+    }
 
     private
 
@@ -29,13 +61,35 @@ module TwelveDaysOfChristmas
       en: {
         gifts: [
           nil,
-          "partridge in a pear tree"
+          "partridge in a pear tree",
+          "turtle doves",
+          "french hens",
+          "calling birds",
+          "gold rings",
+          "geese-a-laying",
+          "swans-a-swimming",
+          "maids-a-milking",
+          "ladies dancing",
+          "lords-a-leaping",
+          "pipers piping",
+          "drummers drumming"
         ]
       },
       en_au: {
         gifts: [
           nil,
-          "kookaburra in a gum tree"
+          "kookaburra in a gum tree",
+          "snakes on skis",
+          "wet galahs",
+          "lyrebirds",
+          "kangaroos",
+          "sharks a-surfing",
+          "emus laying",
+          "dingoes dancing",
+          "crocs a-snoozing",
+          "wombats washing",
+          "lizards leaping",
+          "possums playing"
         ]
       }
     }
--- Ruby/spec/twelve_days_of_christmas/twelve_days_of_christmas_spec.rb
+++ Ruby/spec/twelve_days_of_christmas/twelve_days_of_christmas_spec.rb
@@ -7,36 +7,84 @@ module TwelveDaysOfChristmas
   RSpec.describe TwelveDaysOfChristmas do
     it "returns the first verse of the song" do
       expect(described_class.new.verse(1)).to eq(
-        "On the first day of Christmas/n" \
-        "My true love gave to me:/n" \
+        "On the first day of Christmas\n" \
+        "My true love gave to me:\n" \
         "A partridge in a pear tree."
       )
     end
 
     it "returns the first Aussie verse of the song" do
       expect(described_class.new(locale: :en_au).verse(1)).to eq(
-        "On the first day of Christmas/n" \
-        "My true love gave to me:/n" \
+        "On the first day of Christmas\n" \
+        "My true love gave to me:\n" \
         "A kookaburra in a gum tree."
       )
     end
 
     it "returns the second verse when asked" do
       expect(described_class.new.verse(2)).to eq(
-        "On the second day of Christmas/n" \
-        "My true love gave to me:/n" \
-        "Two turtle doves,/n" \
+        "On the second day of Christmas\n" \
+        "My true love gave to me:\n" \
+        "Two turtle doves,\n" \
         "And a partridge in a pear tree."
       )
     end
 
     it "returns the third verse when asked" do
       expect(described_class.new.verse(3)).to eq(
-        "On the third day of Christmas/n" \
-        "My true love gave to me:/n" \
-        "Three french hens,/n" \
-        "Two turtle doves,/n" \
+        "On the third day of Christmas\n" \
+        "My true love gave to me:\n" \
+        "Three french hens,\n" \
+        "Two turtle doves,\n" \
         "And a partridge in a pear tree."
+      )
+    end
+
+    it "returns the third Aussie verses of the song" do
+      expect(described_class.new(locale: :en_au).verse(3)).to eq(
+        "On the third day of Christmas\n" \
+        "My true love gave to me:\n" \
+        "Three wet galahs,\n" \
+        "Two snakes on skis,\n" \
+        "And a kookaburra in a gum tree."
+      )
+    end
+
+    it "returns the Twelve Aussie verses of the song" do
+      expect(described_class.new(locale: :en_au).verse(12)).to eq(
+        "On the twelfth day of Christmas\n" \
+        "My true love gave to me:\n" \
+        "Twelve possums playing,\n" \
+        "Eleven lizards leaping,\n" \
+        "Ten wombats washing,\n" \
+        "Nine crocs a-snoozing,\n" \
+        "Eight dingoes dancing,\n" \
+        "Seven emus laying,\n" \
+        "Six sharks a-surfing,\n" \
+        "Five kangaroos,\n" \
+        "Four lyrebirds,\n" \
+        "Three wet galahs,\n" \
+        "Two snakes on skis,\n" \
+        "And a kookaburra in a gum tree."
+      )
+    end
+
+    it "returns the Twelve English verses of the song" do
+      expect(described_class.new(locale: :en).verse(12)).to eq(
+        "On the twelfth day of Christmas\n" \
+        "My true love gave to me:\n" \
+        "Twelve drummers drumming,\n" \
+        "Eleven pipers piping,\n" \
+        "Ten lords-a-leaping,\n" \
+        "Nine ladies dancing,\n" \
+        "Eight maids-a-milking,\n" \
+        "Seven swans-a-swimming,\n" \
+        "Six geese-a-laying,\n" \
+        "Five gold rings,\n" \
+        "Four calling birds,\n" \
+        "Three french hens,\n" \
+        "Two turtle doves,\n" \
+        "And a partridge in a pear tree." \
       )
     end
   end
