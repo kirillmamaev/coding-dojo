b1db480fccad85847c7f02ee262780065b2ee54a
Kirill Mamaev <kirill.mamaev@mable.com.au>
2024-05-04 01:39:14 +1000

    Extract DICTIONARY into a separate file

---
+++ Ruby/Lib/twelve_days_of_christmas/dictionary.rb
@@ -0,0 +1,70 @@
+DICTIONARY = {
+  en: {
+    days_of_christmas_line: [
+      nil,
+      "on the first day of Christmas",
+      "on the second day of Christmas",
+      "on the third day of Christmas",
+      "on the fourth day of Christmas",
+      "on the fifth day of Christmas",
+      "on the sixth day of Christmas",
+      "on the seventh day of Christmas",
+      "on the eighth day of Christmas",
+      "on the ninth day of Christmas",
+      "on the tenth day of Christmas",
+      "on the eleventh day of Christmas",
+      "on the twelfth day of Christmas"
+    ],
+    present_giver_line: "my true love gave to me:",
+    conjunction: "and",
+    gifts: [
+      nil,
+      "a partridge in a pear tree",
+      "two turtle doves",
+      "three french hens",
+      "four calling birds",
+      "five gold rings",
+      "six geese-a-laying",
+      "seven swans-a-swimming",
+      "eight maids-a-milking",
+      "nine ladies dancing",
+      "ten lords-a-leaping",
+      "eleven pipers piping",
+      "twelve drummers drumming"
+    ]
+  },
+  en_au: {
+    days_of_christmas_line: [
+      nil,
+      "on the first day of Christmas",
+      "on the second day of Christmas",
+      "on the third day of Christmas",
+      "on the fourth day of Christmas",
+      "on the fifth day of Christmas",
+      "on the sixth day of Christmas",
+      "on the seventh day of Christmas",
+      "on the eighth day of Christmas",
+      "on the ninth day of Christmas",
+      "on the tenth day of Christmas",
+      "on the eleventh day of Christmas",
+      "on the twelfth day of Christmas"
+    ],
+    present_giver_line: "my true love gave to me:",
+    conjunction: "and",
+    gifts: [
+      nil,
+      "a kookaburra in a gum tree",
+      "two snakes on skis",
+      "three wet galahs",
+      "four lyrebirds",
+      "five kangaroos",
+      "six sharks a-surfing",
+      "seven emus laying",
+      "eight dingoes dancing",
+      "nine crocs a-snoozing",
+      "ten wombats washing",
+      "eleven lizards leaping",
+      "twelve possums playing"
+    ]
+  }
+}.freeze
