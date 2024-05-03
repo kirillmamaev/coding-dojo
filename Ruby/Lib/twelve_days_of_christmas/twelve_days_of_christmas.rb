ee378442e1f1f8a6d0ce86fbb88a5c7e3858e101
Kirill Mamaev <kirill.mamaev@mable.com.au>
2024-05-04 02:05:45 +1000

    Refactor code according to Ruby best practices

--- Ruby/Lib/twelve_days_of_christmas/twelve_days_of_christmas.rb
+++ Ruby/Lib/twelve_days_of_christmas/twelve_days_of_christmas.rb
@@ -5,10 +5,13 @@ require 'twelve_days_of_christmas/dictionary'
 module TwelveDaysOfChristmas
   class SongGenerator
     FIRST_DAY = 1
+    SECOND_DAY = 2
     LAST_DAY = 12
     COMMA =","
     DOT = "."
     EOL = "\n"
+
+    attr_reader :locale
 
     def initialize(locale: :en)
       @locale = locale
@@ -24,15 +27,15 @@ module TwelveDaysOfChristmas
     def verse(day_number)
       output = capitalize("#{DICTIONARY[@locale][:days_of_christmas_line][day_number]}#{EOL}")
       output += capitalize("#{DICTIONARY[@locale][:present_giver_line]}#{EOL}")
+      return "#{output}#{capitalize(DICTIONARY[@locale][:gifts][FIRST_DAY])}#{DOT}#{EOL}" if day_number == FIRST_DAY
 
-      day_number.downto(2) do |gift_number|
+      day_number.downto(SECOND_DAY) do |gift_number|
         output += capitalize("#{DICTIONARY[@locale][:gifts][gift_number]}#{COMMA}#{EOL}")
       end
 
-      output += capitalize(
-        ((day_number == 1) ? "" : "#{DICTIONARY[@locale][:conjunction]} ") +
-        "#{DICTIONARY[@locale][:gifts][1]}#{DOT}#{EOL}"
-      )
+      output + "#{capitalize(
+          "#{DICTIONARY[locale][:conjunction]} #{DICTIONARY[locale][:gifts][FIRST_DAY]}"
+        )}#{DOT}#{EOL}"
     end
 
     private
