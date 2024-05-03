cd8957f8815d1684ca06d29f15c84bebd07f33d4
Kirill Mamaev <kirill.mamaev@mable.com.au>
2024-05-04 01:40:36 +1000

    Refactor to fully support multiple locales

--- Ruby/Lib/twelve_days_of_christmas/twelve_days_of_christmas.rb
+++ Ruby/Lib/twelve_days_of_christmas/twelve_days_of_christmas.rb
@@ -1,101 +1,43 @@
 # frozen_string_literal: true
 
+require 'twelve_days_of_christmas/dictionary'
+
 module TwelveDaysOfChristmas
-  class TwelveDaysOfChristmas
-    DICTIONARY = {
-      numbers: [
-        nil,
-        nil,
-        "Two",
-        "Three",
-        "Four",
-        "Five",
-        "Six",
-        "Seven",
-        "Eight",
-        "Nine",
-        "Ten",
-        "Eleven",
-        "Twelve"
-      ],
-      numerals: [
-        nil,
-        "first",
-        "second",
-        "third",
-        "fourth",
-        "fifth",
-        "sixth",
-        "seventh",
-        "eighth",
-        "ninth",
-        "tenth",
-        "eleventh",
-        "twelfth"
-      ],
-      en: {
-        gifts: [
-          nil,
-          "partridge in a pear tree",
-          "turtle doves",
-          "french hens",
-          "calling birds",
-          "gold rings",
-          "geese-a-laying",
-          "swans-a-swimming",
-          "maids-a-milking",
-          "ladies dancing",
-          "lords-a-leaping",
-          "pipers piping",
-          "drummers drumming"
-        ]
-      },
-      en_au: {
-        gifts: [
-          nil,
-          "kookaburra in a gum tree",
-          "snakes on skis",
-          "wet galahs",
-          "lyrebirds",
-          "kangaroos",
-          "sharks a-surfing",
-          "emus laying",
-          "dingoes dancing",
-          "crocs a-snoozing",
-          "wombats washing",
-          "lizards leaping",
-          "possums playing"
-        ]
-      }
-    }
+  class SongGenerator
+    FIRST_DAY = 1
+    LAST_DAY = 12
+    EOL = "\n"
 
     def initialize(locale: :en)
       @locale = locale
     end
 
-    def verse(number)
-      output = generate_starting_verse_lines(number)
-      (number..2).step(-1).each do |i|
-        output += "#{DICTIONARY[:numbers][i]} #{DICTIONARY[@locale][:gifts][i]},\n" \
+    def verse(day_number)
+      output = capitalize("#{DICTIONARY[@locale][:days_of_christmas_line][day_number]}#{EOL}")
+      output += capitalize("#{DICTIONARY[@locale][:present_giver_line]}#{EOL}")
+
+      day_number.downto(2) do |gift_number|
+        output += capitalize("#{DICTIONARY[@locale][:gifts][gift_number]},#{EOL}")
       end
-      output +=
-        ((number == 1) ? "A " : "And a ") +
-        "#{DICTIONARY[@locale][:gifts][1]}."
+
+      output += capitalize(
+        ((day_number == 1) ? "" : "#{DICTIONARY[@locale][:conjunction]} ") +
+        "#{DICTIONARY[@locale][:gifts][1]}.#{EOL}"
+      )
     end
 
     def song
       output = ''
       (1..12).each do |i|
-        output += verse(i) + ((i == 12) ? "\n" : "\n\n")
+        output += "#{verse(i)}#{((i == 12) ? "" : "#{EOL}")}"
       end
       output
     end
 
     private
 
-    def generate_starting_verse_lines(number)
-      "On the #{DICTIONARY[:numerals][number]} day of Christmas\n" \
-      "My true love gave to me:\n" \
+    def capitalize(string)
+      "#{string[0].upcase}#{string[1..-1]}"
     end
   end
 end
