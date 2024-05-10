80cb4f88107c3b0589731bb50a6045be5c68d1ce
Kirill Mamaev <kirill.mamaev@mable.com.au>
2024-05-10 12:58:33 +1000

    Fix linter offences.

--- Ruby/spec/twelve_days_of_christmas/twelve_days_of_christmas_spec.rb
+++ Ruby/spec/twelve_days_of_christmas/twelve_days_of_christmas_spec.rb
@@ -7,13 +7,13 @@ require "twelve_days_of_christmas/dictionary"
 module TwelveDaysOfChristmas
   RSpec.describe SongGenerator do
     before(:all) do
-      @TWELVE_DAYS_OF_CHRISTMAS_SONG_EN = File.read(
+      @twelve_days_of_christmas_song_en = File.read(
         File.join(
           File.dirname(__FILE__), "twelve_days_of_shristmas_en.txt"
         )
       ).freeze
 
-      @TWELVE_DAYS_OF_CHRISTMAS_SONG_EN_AU = File.read(
+      @twelve_days_of_christmas_song_en_au = File.read(
         File.join(
           File.dirname(__FILE__), "twelve_days_of_christmas_en_au.txt"
         )
@@ -22,64 +22,68 @@ module TwelveDaysOfChristmas
 
     it "returns the first verse of the english song" do
       expect(described_class.new(locale: :en).verse(1)).to eq(
-        @TWELVE_DAYS_OF_CHRISTMAS_SONG_EN.split("\n\n")[0] +
+        @twelve_days_of_christmas_song_en.split("\n\n")[0] +
         "\n"
       )
     end
 
     it "returns the first verse of the aussie song" do
       expect(described_class.new(locale: :en_au).verse(1)).to eq(
-        @TWELVE_DAYS_OF_CHRISTMAS_SONG_EN_AU.split("\n\n")[0] +
+        @twelve_days_of_christmas_song_en_au.split("\n\n")[0] +
          "\n"
       )
     end
 
     it "returns the second verse of the english song" do
       expect(described_class.new(locale: :en).verse(2)).to eq(
-        @TWELVE_DAYS_OF_CHRISTMAS_SONG_EN.split("\n\n")[1] +
+        @twelve_days_of_christmas_song_en.split("\n\n")[1] +
         "\n"
       )
     end
 
     it "returns the second verse of the aussie song" do
       expect(described_class.new(locale: :en_au).verse(2)).to eq(
-        @TWELVE_DAYS_OF_CHRISTMAS_SONG_EN_AU.split("\n\n")[1] +
+        @twelve_days_of_christmas_song_en_au.split("\n\n")[1] +
         "\n"
       )
     end
 
     it "returns the third verse of the english song" do
       expect(described_class.new(locale: :en).verse(3)).to eq(
-        @TWELVE_DAYS_OF_CHRISTMAS_SONG_EN.split("\n\n")[2] +
+        @twelve_days_of_christmas_song_en.split("\n\n")[2] +
         "\n"
       )
     end
 
     it "returns the third verse of the aussie song" do
       expect(described_class.new(locale: :en_au).verse(3)).to eq(
-        @TWELVE_DAYS_OF_CHRISTMAS_SONG_EN_AU.split("\n\n")[2] +
+        @twelve_days_of_christmas_song_en_au.split("\n\n")[2] +
         "\n"
       )
     end
 
     it "returns the twelves verse of the english song" do
       expect(described_class.new(locale: :en).verse(12)).to eq(
-        @TWELVE_DAYS_OF_CHRISTMAS_SONG_EN.split("\n\n")[11]
+        @twelve_days_of_christmas_song_en.split("\n\n")[11]
       )
     end
 
     it "returns the twelves verse of the aussie song" do
       expect(described_class.new(locale: :en_au).verse(12)).to eq(
-        @TWELVE_DAYS_OF_CHRISTMAS_SONG_EN_AU.split("\n\n")[11]
+        @twelve_days_of_christmas_song_en_au.split("\n\n")[11]
       )
     end
 
     it "returns the whole classic english song" do
-      expect(described_class.new(locale: :en).song).to eq(@TWELVE_DAYS_OF_CHRISTMAS_SONG_EN)
+      expect(described_class.new(locale: :en).song).to eq(
+        @twelve_days_of_christmas_song_en
+      )
     end
 
     it "returns the whole classic aussie song" do
-      expect(described_class.new(locale: :en_au).song).to eq(@TWELVE_DAYS_OF_CHRISTMAS_SONG_EN_AU)
+      expect(described_class.new(locale: :en_au).song).to eq(
+        @twelve_days_of_christmas_song_en_au
+      )
     end
   end
 end
